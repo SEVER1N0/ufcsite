62267ad3d37b10de81d1e9811dd9b13fc5c3f8d4
GU1NHO <thiagorosacarvalho@usp.br>
2024-11-17 19:43:50 -0300

    Quarta tentativa Render

--- manage.py
+++ manage.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
+def create_default_admin():
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        print('Superusuário padrão criado.')
+    else:
+        print('Superusuário padrão já existe.')
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ufcsite.settings')
     try:
-        from django.core.management import execute_from_command_line
+        # Inicializa o Django antes de criar o superusuário
+        import django
+        django.setup()
+        create_default_admin()
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
