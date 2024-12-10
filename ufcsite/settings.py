d895a88a9d4cc62b27f5622492a51ef52da27540
SEVER1N0 <severino.pipoca@gmail.com>
2024-12-10 11:56:17 -0300

    Agora vai 3

--- ufcsite/settings.py
+++ ufcsite/settings.py
@@ -83,11 +83,16 @@ WSGI_APPLICATION = 'ufcsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
